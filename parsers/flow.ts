075dd130e5ecc006e7b0610df9ee91a581f957bc
Pionxzh <pionxzh@csie.io>
2024-07-25 03:47:16 +0800

    chore: fix type infer error

--- parsers/flow.ts
+++ parsers/flow.ts
@@ -8,7 +8,7 @@ import getBabelOptions, { Overrides } from "./_babel_options";
 //     parser: require("recast/parsers/flow")
 //   });
 //
-export function parse(source: string, options?: Overrides) {
+export function parse(source: string, options?: Overrides): ReturnType<typeof parser.parse> {
   const babelOptions = getBabelOptions(options);
   babelOptions.plugins.push("jsx", "flow");
   return parser.parse(source, babelOptions);
--- parsers/typescript.ts
+++ parsers/typescript.ts
@@ -8,7 +8,7 @@ import getBabelOptions, { Overrides } from "./_babel_options";
 //     parser: require("recast/parsers/typescript")
 //   });
 //
-export function parse(source: string, options?: Overrides) {
+export function parse(source: string, options?: Overrides): ReturnType<typeof parser.parse> {
   const babelOptions = getBabelOptions(options);
   babelOptions.plugins.push("typescript");
   return parser.parse(source, babelOptions);
